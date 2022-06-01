0ae5db3b21d7f14c94856e405f2c11020578c81f
Santiago Cartagena <scartagena147@gmail.com>
2022-06-01 20:02:58 -0300

    Splash login main activities navigation

---
+++ app/src/main/java/edu/ort/tp3parcial_a/SplashActivity.kt
@@ -0,0 +1,26 @@
+package edu.ort.tp3parcial_a
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var splashButton : Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+
+    override fun onStart(){
+        super.onStart()
+        splashButton = findViewById(R.id.splash_button)
+        val intention = Intent(this, LoginActivity::class.java)
+        splashButton.setOnClickListener {
+            startActivity(intention)
+        }
+    }
+
+}
