2ef84496f681b7bc7d9f5799282a27f7118bfc89
Santiago Cartagena <scartagena147@gmail.com>
2022-06-01 23:15:19 -0300

    Changed app to work with bottom nav and drawer fix

--- app/src/main/java/edu/ort/ejemplonavcoso/MainActivity.kt
+++ app/src/main/java/edu/ort/ejemplonavcoso/MainActivity.kt
@@ -35,18 +35,12 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow
+                R.id.nav_home, R.id.nav_notifications, R.id.nav_settings
             ), drawerLayout
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
         navBottomView.setupWithNavController(navController)
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
     }
 
     override fun onSupportNavigateUp(): Boolean {
