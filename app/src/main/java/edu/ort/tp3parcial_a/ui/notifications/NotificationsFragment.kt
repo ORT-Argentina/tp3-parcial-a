cd9540377a0a745017443f5031d87a72544149db
Santiago Cartagena <scartagena147@gmail.com>
2022-06-01 21:05:22 -0300

    Notifications recyclerview

--- app/src/main/java/edu/ort/tp3parcial_a/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/edu/ort/tp3parcial_a/ui/notifications/NotificationsFragment.kt
@@ -7,11 +7,17 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import edu.ort.tp3parcial_a.R
+import edu.ort.tp3parcial_a.adapters.NotificationsListAdapter
 import edu.ort.tp3parcial_a.databinding.FragmentNotificationsBinding
 
 class NotificationsFragment : Fragment() {
 
     private var _binding: FragmentNotificationsBinding? = null
+    private lateinit var recycler : RecyclerView
+    private lateinit var notificationsListAdapter : NotificationsListAdapter
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -27,12 +33,23 @@ class NotificationsFragment : Fragment() {
 
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
-        val textView: TextView = binding.textNotifications
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        recycler = root.findViewById(R.id.notifications_recycler)
         return root
+
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        var notificaciones : MutableList<String> = ArrayList<String>()
+        for(i in 1..10){
+            notificaciones.add(i.toString())
+        }
+
+        recycler.setHasFixedSize(true)
+        recycler.layoutManager = LinearLayoutManager(context)
+        notificationsListAdapter = NotificationsListAdapter(notificaciones)
+        recycler.adapter = notificationsListAdapter
     }
 
     override fun onDestroyView() {
